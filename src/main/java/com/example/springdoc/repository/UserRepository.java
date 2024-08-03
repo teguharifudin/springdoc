9a5239b732cb88f19c5b1d00bd070196b41b836f
Teguh Arief <114749743+teguharifudin@users.noreply.github.com>
2024-08-03 23:38:46 +0700

    Update UserRepository.java

--- src/main/java/com/example/springdoc/repository/UserRepository.java
+++ src/main/java/com/example/springdoc/repository/UserRepository.java
@@ -9,7 +9,5 @@ import com.example.springdoc.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-
-	//emai used for username - use email for username authentication
 	Optional<User> findByEmail(String email);
 }
